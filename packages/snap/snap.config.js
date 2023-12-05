d45868cd5d2da71b20f70069d61a7f8c95e9f19e
Porco <porcorossoj89@gmail.com>
2023-12-05 15:48:46 +0400

    first

---
+++ packages/snap/snap.config.js
@@ -0,0 +1,90 @@
+const { merge } = require('@metamask/snaps-cli');
+const { EsbuildPlugin } = require('esbuild-loader');
+const { resolve } = require('path');
+const ResolveTypeScriptPlugin = require('resolve-typescript-plugin');
+
+module.exports = {
+  bundler: 'webpack',
+  sourceMap: true,
+  evaluate: true,
+  input: './src/index.ts',
+  manifest: {
+    path: './snap.manifest.json',
+    update: true,
+  },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  server: {
+    enabled: true,
+    port: 8081,
+  },
+  experimental: {
+    wasm: true,
+  },
+  environment: {
+    NODE_DEBUG: false,
+    NODE_ENV: 'production',
+    DEBUG: false,
+  },
+  stats: {
+    verbose: false,
+  },
+  polyfills: {
+    buffer: true,
+    stream: true,
+    tty: true,
+    util: true,
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  customizeWebpackConfig: (config) =>
+    merge(config, {
+      module: {
+        rules: [
+          {
+            test: /\.(cjs|js|mjs|ts)$/u,
+            // exclude: [/node_modules/u],
+            loader: 'o',
+            options: {
+              target: 'es2022',
+              tsconfig: './tsconfig.json',
+              sourcemap: true,
+            },
+          },
+        ],
+      },
+
+      // target: 'web',
+      // mode: 'production',
+
+      resolve: {
+        plugins: [new ResolveTypeScriptPlugin()],
+        extensions: ['.ts', '.js', '.cjs', '.mjs'],
+        alias: {
+          './node/index.js': false,
+        },
+        fallback: {
+          crypto: false,
+          os: false,
+          fs: false,
+          path: false,
+          url: false,
+          events: false,
+          buffer: require.resolve('buffer/'),
+        },
+      },
+      optimization: {
+        minimize: true,
+        minimizer: [
+          new EsbuildPlugin({
+            target: 'es2022',
+            css: true,
+          }),
+        ],
+      },
+    }),
+};
+
+// export default config;
--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,52 +1,53 @@
-import { getSchnorrAccount } from '@aztec/aztec.js';
-import { GrumpkinScalar } from '@aztec/foundation'
-// import { GrumpkinPrivateKey } from '@aztec/types';
+import {
+  createPXEClient,
+  Fr,
+  GrumpkinScalar,
+  getSandboxAccountsWallets,
+} from '@aztec/aztec.js';
 import type { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+import { PXE_URL } from './constants';
+// import { GrumpkinPrivateKey } from '@aztec/types';
+// import { panel, text } from '@metamask/snaps-ui';
 
+export async function getAddress(): Promise<string> {
+  // const pk =
+  //   0x2153536ff6628eee01cf4024889ff977a18d9fa61d0e414422f7681cf085c281n;
+  // const encryptionPrivateKey = new GrumpkinScalar(new Fr(pk).fromS);
+  // const encryptionPrivateKey: GrumpkinScalar = GrumpkinScalar.random();
+  // console.log('encryptionPrivateKey: ', encryptionPrivateKey);
+  const pxe = createPXEClient(PXE_URL);
+  // const pxe = createPXEClient(PXE_URL, makeFetch([1, 2, 3, 4, 5], true));
+  console.log('pxe: ', pxe);
 
-const pk = '0x2153536ff6628eee01cf4024889ff977a18d9fa61d0e414422f7681cf085c281';
-const encryptionPrivateKey = new GrumpkinScalar(new Fr(pk));
+  console.log('node info: ', await pxe.getNodeInfo());
+  // console.log('waitForSandbox: ', waitForSandbox(pxe));
+  const accountWallets = await getSandboxAccountsWallets(pxe);
+  console.log('accountWallets: ', accountWallets);
 
-// console.log('as');
-
-// getAddress();
-
-
-
-export const getAddress = async (): Promise<string> => {
-  const pxe = await createPXEClient('http://localhost:8080');
-
-  // const encryptionPrivateKey = GrumpkinPrivateKey.random();
-  // const signingPrivateKey = GrumpkinPrivateKey.random();
-  // const wallet = getSchnorrAccount(pxe, encryptionPrivateKey, signingPrivateKey).waitDeploy();
-  // console.log(`New account deployed at ${wallet.getAddress()}`);
-
-
-  console.log('1');
-  // const pxe = await createPXEClient('http://localhost:8080');
-  // console.log('2');
-  // const accountContract = new SingleKeyAccountContract(encryptionPrivateKey);
-  // console.log('3');
+  // console.log('node info: ', await pxe.getNodeInfo());
+  // const accountContract = new SchnorrAccountContract(encryptionPrivateKey);
+  // console.log('accountContract: ', accountContract);
   // const accountManager = new AccountManager(
   //   pxe,
   //   encryptionPrivateKey,
   //   accountContract,
   // );
   // console.log('4');
+  // console.log('accountManager: ', accountManager);
 
-  // const wallet = await accountManager.getWallet();
-
+  // // const wallet = await accountManager.getWallet();
+  // const wallet = await accountManager.waitDeploy();
   // console.log('5');
-  // // const votingContract = await VotingContract.at(
-  // //   '0x1254268ac4d842d5c3acd9e555015b13e2785a3e51b3272cd6578534e59d1c87',
-  // //   wallet,
-  // // );
-  // // console.log('6');
-  // // const adminAddr = await votingContract.methods.admin().view();
-  // return wallet.getAddress();
-};
-
+  // console.log('wallet: ', wallet);
+  // const votingContract = await VotingContract.at(
+  //   '0x149a9593f1ca604b7aeb9c8d7732b872b84b358ac765e4a03af2093d2cb6da0e',
+  //   wallet,
+  // );
+  console.log('6');
+  // const adminAddr = await votingContract.methods.admin().view();
+  // return adminAddr.toString();
+  return accountWallets[0]?.getAddress().toString();
+}
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
  *
@@ -57,23 +58,31 @@ export const getAddress = async (): Promise<string> => {
  * @returns The result of `snap_dialog`.
  * @throws If the request method is not valid for this snap.
  */
-export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
+export const onRpcRequest: OnRpcRequestHandler = async ({
+  origin,
+  request,
+}) => {
+  // switch (request.method) {
+  //   case 'hello':
+  //     return snap.request({
+  //       method: 'snap_dialog',
+  //       params: {
+  //         type: 'confirmation',
+  //         content: panel([
+  //           text(`Hello, **${origin}**!`),
+  //           text('This custom confirmation is just for display purposes.'),
+  //           text(
+  //             'But you can edit the snap source code to make it do something, if you want to!',
+  //           ),
+  //         ]),
+  //       },
+  //     });
+  //   default:
+  //     throw new Error('Method not found.');
+  // }
   switch (request.method) {
     case 'hello':
-      await getAddress();
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
-      });
+      return await getAddress();
     default:
       throw new Error('Method not found.');
   }
